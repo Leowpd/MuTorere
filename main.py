50a3d3ff0862c059d369e59aedb5fc9f1a6e8042
Leo Duncan <leo.wouter.duncan@gmail.com>
2021-10-13 14:43:42 +1300

    draws one kewai

--- main.py
+++ main.py
@@ -3,9 +3,39 @@ import turtle, random, time
 screen = turtle.Screen()
 screen.setup(700, 700)
 
+#draws the board
+def drawboard():
+    #draws one kewai
+    def kewai():
+        t.pd()
+        t.rt(172)
+        t.fd(150)
+        t.pu()
+        t.fd(10)
+        t.pd()
+        t.rt(98)
+        t.pu()
+        t.fd(44.5)
+        t.pd()
+        t.rt(98)
+        t.pu()
+        t.fd(10)
+        t.pd()
+        t.fd(150)
+    t = turtle.Turtle(visible=False)
+    t.width(2)
+    t.speed(0)
+    t.color("black")
+    t.lt(90)
+    t.pu()
+    t.fd(250)
+    t.pd()
+
+    kewai()
 
 
 
+drawboard()
 
 
 
