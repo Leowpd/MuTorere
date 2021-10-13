35d299a38a5c02a91ce3feb63ab5743cdbf76552
Leo Duncan <leo.wouter.duncan@gmail.com>
2021-10-13 14:24:41 +1300

    essential changes

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 # cgvhbnm8 6bhgvfu8,kymgntbhjihbgnjmioucgnbft6hujok8m, j.bugyfxdcghjiuhygtfdxvcv hbjkhugyftdxcvhbjk.ihugytfdfcgvhjokpilgyfcgvhbjiohu,gfncbdxbcghjkoj.h,gcfvghbjkh,gncfx bvbhjhmvg bj\
-print ("ur mom ")
+print ("ur computer's dumb ")
+
+x = 100000000000000
