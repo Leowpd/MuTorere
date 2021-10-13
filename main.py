7013fa87ca81fdf57b3227558eb25ed60e1dd679
Leo Duncan <leo.wouter.duncan@gmail.com>
2021-10-13 14:41:11 +1300

    First Proper Commit, Setup

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-# cgvhbnm8 6bhgvfu8,kymgntbhjihbgnjmioucgnbft6hujok8m, j.bugyfxdcghjiuhygtfdxvcv hbjkhugyftdxcvhbjk.ihugytfdfcgvhjokpilgyfcgvhbjiohu,gfncbdxbcghjkoj.h,gcfvghbjkh,gncfx bvbhjhmvg bj\
-print ("ur computer's dumb ")
+import turtle, random, time
+#sets up screen
+screen = turtle.Screen()
+screen.setup(700, 700)
 
-x = 100000000000000
 
-#okay better actually do something now
+
+
+
+
+
+
+
+
+# just for testing, makes it so the window doesnt close by itself.
+screen.exitonclick()
