73f1d7b55e32d9dd01109aff1844d8889a755347
Leo Duncan <leo.wouter.duncan@gmail.com>
2021-10-16 22:46:28 +1300

    is_valid_move function almost works

--- testshit.py
+++ testshit.py
@@ -6,21 +6,17 @@ COLUMN_COUNT = 3
 def create_board():
     board = np.zeros((3,3))
     board[0][0] = 1
+    board[0][1] = 1
+    board[0][2] = 1
     board[1][0] = 1
+    board[1][1] = 0 #centre
+    board[1][2] = 1
     board[2][0] = 1
     board[2][1] = 1
-    board[0][1] = 2
-    board[0][2] = 2
-    board[1][2] = 2
-    board[2][2] = 2
+    board[2][2] = 1
 
     return board
 
-def move_piece(board, row, col, player):
-    board[row][col] = player
-
-def remove_piece(board, row, col):
-    board[row][col] = 0
 
 def is_valid_move(row, col, empty_row, empty_col):
         if row == empty_row and col == empty_col:
@@ -46,7 +42,7 @@ def is_valid_move(row, col, empty_row, empty_col):
         else:
             #return False
             print("no") #testing
-    
+
 
 def find_empty_pos(board):
     empty_pos = np.where(board == 0)
@@ -57,41 +53,14 @@ def find_empty_pos(board):
 board = create_board()
 print(board)
 game_over = False
-turn = 0
 
 
 while not game_over:
-    # Ask for player 1 where to move
-    if turn == 0:
-        empty_row, empty_col = find_empty_pos(board)
-        print(empty_row)
-        print(empty_col)
+    empty_row, empty_col = find_empty_pos(board)
+    print(empty_row)
+    print(empty_col)
 
-        row = int(input("Player 1 select row (0-2): "))
-        col = int(input("Player 1 select column (0-2): "))
+    row = int(input("Player 1 select row (0-2): "))
+    col = int(input("Player 1 select column (0-2): "))
 
-        is_valid_move(board, row, col, empty_row, empty_col)
-
-        # if is_valid_move(board, row, col, empty_row, empty_col):
-        #     #move_piece(board, row, col, 1)
-        #     pass
-
-        # else:
-        #     print("invalid move")
-
-
-    # Ask for player 2 where to move
-    else:
-        pass
-        row = int(input("Player 2 select row (0-2): "))
-        col = int(input("Player 2 select column (0-2): "))
-
-        if is_valid_move(board, row, col):
-            move_piece(board, row, col, 2)
-        else:
-            print("invalid move")
-
-    print(board)
-
-    turn += 1
-    turn = turn % 2
+    is_valid_move(row, col, empty_row, empty_col) #note, doesnt check which tile the player is moving, make sure to add that player 1 can only move "1" tiles and player 2 can only move "2" tiles.
