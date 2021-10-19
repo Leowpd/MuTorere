d81c01833051900f9fddc81d04607576e2738807
Leo Duncan <leo.wouter.duncan@gmail.com>
2021-10-19 17:07:14 +1300

    is_valid_move function complete+working, fixed bug

--- logictesting.py
+++ logictesting.py
@@ -52,37 +52,64 @@ def move_piece(board, empty_row, empty_col, player):
 def remove_piece(board, row, col):
     board[row][col] = 0
 
-def is_valid_move(row, col, empty_row, empty_col, player, piece):
-    if player == piece:
-        if row == empty_row and col == empty_col:                   #this may be able to be deleted
-            return False                                            #this may be able to be deleted
-            #print("no, this is already the empty space") #testing  #this may be able to be deleted
-        elif empty_row == 1 and empty_col == 1:
+#is the function for checking if a move is valid
+def is_valid_move(board, row, col, empty_row, empty_col, player, piece_num):
+    #checks if the piece a player is trying to move is actually their piece
+    if player == piece_num:
+        #checks if the player's piece is in the middle
+        if row == 1 and col == 1:
             return True
-            #print("yes, empty is at centre") #testing
-        elif row == 1 and col == 1:
-            return True
-            #print("yes, player piece is at centre") #testing
-        elif col == empty_col or col + 1 == empty_col or col - 1 == empty_col:
-            if row == empty_row or row + 1 == empty_row or row - 1 == empty_row:
-                if col == empty_col or row == empty_row:
+        else:
+            #checks if a player's piece is surrounded by two other of the same piece on the outside (so not including the middle piece)
+            player_pieces = np.nonzero(board == player)
+            list_of_player_pieces = list(zip(player_pieces[0], player_pieces[1]))
+            adjacent = 0
+            #checks to see if any of the player's other pieces is adjacent to the piece they are trying to move
+            for piece in list_of_player_pieces:
+                new_row = int(piece[0])
+                new_col = int(piece[1])
+                #removes the selected piece from the pieces being compared
+                if col == new_col and row == new_row:
+                    pass
+                else:
+                    #checks to see if the piece is adjacent to the selected piece
+                    if col == new_col or col + 1 == new_col or col - 1 == new_col:
+                        if row == new_row or row + 1 == new_row or row - 1 == new_row:
+                            if row == new_row or col == new_col:
+                                if not (new_row == 1 and new_col == 1):
+                                    adjacent += 1
+            #if there are two adjacent pieces then the selected piece cannot be moved
+            if adjacent >= 2:
+                return False
+            else:
+                #checks if the empty space is at the centre/putahi
+                if empty_row == 1 and empty_col == 1:
                     return True
-                    #print("yes, adjacent") #testing
+                #checks if the empty space is adjacent to the piece the player wants to move
+                elif col == empty_col or col + 1 == empty_col or col - 1 == empty_col:
+                    if row == empty_row or row + 1 == empty_row or row - 1 == empty_row:
+                        if col == empty_col or row == empty_row:
+                            return True
+                        else:
+                            return False
+                            #the empty space is diagonally adjacent which is not valid
+                            #this is here so if in the future I want to add specific error messages for why an input doesn't work, it is easy. This increases the flexibility of the program
+                    else:
+                        return False
+                        #the empty space is not adjacent to the piece the player is trying to move
+                        #this is here so if in the future I want to add specific error messages for why an input doesn't work, it is easy. This increases the flexibility of the program
                 else:
                     return False
-                    #print("no, diagonal") #testing
-            else:
-                return False
-                #print("no") #testing
-        else:
-            return False
-            #print("no") #testing
-    elif piece == 0:
+                    #the empty space is not adjacent to the piece the player is trying to move
+                    #this is here so if in the future I want to add specific error messages for why an input doesn't work, it is easy. This increases the flexibility of the program
+    #checks if the piece the player is trying to move is actually the empty space
+    elif piece_num == 0:
         return False
-        #print("no, this is already the empty space") #testing
+        #selected spot is empty
+        #this is here so if in the future I want to add specific error messages for why an input doesn't work, it is easy. This increases the flexibility of the program.
     else:
         return False
-        #print("no, you can not move your opponent's piece")
+            #if it gets to here the player has selected their oppenent's piece, which of course is not valid
     
 def find_empty_pos(board):
     empty_pos = np.where(board == 0)
@@ -101,12 +128,11 @@ def winning_move(board, opponent):
         #print(piece)
         row = int(piece[0])
         col = int(piece[1])
-        if not is_valid_move(row, col, empty_row, empty_col, player=opponent, piece=opponent):
+        if not is_valid_move(board, row, col, empty_row, empty_col, player=opponent, piece_num=opponent):
             #print("haha nope")
             opp_loses += 1
             #print("opp_loses", opp_loses)
         else:
-            print("yup all good")
             pass
 
     #print("final opp_loses", opp_loses)
@@ -129,19 +155,11 @@ while not game_over:
         opponent = 2
         empty_row, empty_col = find_empty_pos(board)
 
-############## this needs to be improved ###################
         row = take_player_input(player, "row")
         col = take_player_input(player, "column")
-        piece = board[row][col]     
+        piece_num = board[row][col]
 
-        while not is_valid_move(row, col, empty_row, empty_col, player, piece):
-            print("INVALID MOVEEEE") #testing
-            row = take_player_input(player, "row")
-            col = take_player_input(player, "column")
-            piece = board[row][col]            
-            is_valid_move(row, col, empty_row, empty_col, player, piece)
-
-        if is_valid_move(row, col, empty_row, empty_col, player, piece):
+        if is_valid_move(board, row, col, empty_row, empty_col, player, piece_num):
             print("IS VALID MOVE") #testing
             move_piece(board, empty_row, empty_col, player)
             remove_piece(board, row, col)
@@ -152,10 +170,10 @@ while not game_over:
                 print("havent won yet lol player 1")
 
         else:
-            print("something fucked up player 1")
+            print("INVALID MOVE")
+            turn -= 1
 
 
-############## this needs to be improved ###################
 
 
     # Ask for player 2 where to move
@@ -164,19 +182,11 @@ while not game_over:
         opponent = 1
         empty_row, empty_col = find_empty_pos(board)
 
-############## this needs to be improved ###################
         row = take_player_input(player, "row")
         col = take_player_input(player, "column")
-        piece = board[row][col]     
+        piece_num = board[row][col]
 
-        while not is_valid_move(row, col, empty_row, empty_col, player, piece):
-            print("INVALID MOVEEEE") #testing
-            row = take_player_input(player, "row")
-            col = take_player_input(player, "column")
-            piece = board[row][col]            
-            is_valid_move(row, col, empty_row, empty_col, player, piece)
-
-        if is_valid_move(row, col, empty_row, empty_col, player, piece):
+        if is_valid_move(board, row, col, empty_row, empty_col, player, piece_num):
             print("IS VALID MOVE") #testing
             move_piece(board, empty_row, empty_col, player)
             remove_piece(board, row, col)
@@ -187,10 +197,10 @@ while not game_over:
                 print("havent won yet lol player 2")
 
         else:
-            print("something fucked up player 2")
+            print("INVALID MOVE")
+            turn -= 1
 
 
-############## this needs to be improved ###################
 
     print(board)
 
